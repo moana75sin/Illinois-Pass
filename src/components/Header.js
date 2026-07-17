8d0a2117ecba13ecc59eeb1764cb81546a3abb5d
moana75sin <moana75sin@outlook.com>
2026-07-17 11:23:15 +0530

    Create Header.js

---
+++ src/components/Header.js
@@ -0,0 +1,30 @@
+import React from "react";
+import "./Header.css";
+
+
+function Header() {
+
+  return (
+
+    <header className="header">
+
+      <div className="header-content">
+
+        <h1>
+          Illinois Pass
+        </h1>
+
+        <p>
+          Smart Digital Expense & Receipt Management
+        </p>
+
+      </div>
+
+    </header>
+
+  );
+
+}
+
+
+export default Header;
